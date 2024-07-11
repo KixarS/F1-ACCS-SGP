e9bcfb974e37883224f241dee980e8e30f8aaf22
KixarS <teoylimhao2@gmail.com>
2024-07-11 20:52:26 +0700

    create navbar and add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import navlogo from "./image/ACCS-Logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='flex justify-center w-full text-xl text-red-500'>
-        หิวข้าว
+    // Home Page
+    <div className="flex w-full h-full">
+      {/* Navigation Bar */}
+      <div className="flex flex-row justify-between items-center w-full h-[79px] bg-white drop-shadow-lg text-base overflow-hidden">
+        {/* Logo */}
+        <div className="flex w-fit h-full flex-row items-center ml-24">
+          <img
+            src={navlogo}
+            alt="accs-logo"
+            className="flex min-w-[100px] max-w-[100px] min-h-[95px] max-h-[95px] cursor-pointer object-contain"
+          />
+        </div>
+        {/* Navbar Content */}
+        <div>content</div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
