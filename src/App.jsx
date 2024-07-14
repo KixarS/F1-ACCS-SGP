e74ade18029d0810e4d602f37907732ff4073a88
KixarS <teoylimhao2@gmail.com>
2024-07-14 21:36:45 +0700

    change picture name and add responsive for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import navlogo from "./image/ACCS-Logo.png";
-import thailogo from "./image/Thai-Logo.png";
-import carouselbg from "./image/Carousel-BG.png";
+import navlogo from "./image/ACCSLogo.png";
+import thailogo from "./image/ThaiLogo.png";
+import carouselBG from "./image/CarouselBG.png";
+import carouselPropOne from "./image/CarouselPropOne.png";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       {/* Navigation Bar */}
       <div className="flex flex-row justify-between items-center w-full h-[79px] bg-white drop-shadow-lg text-base overflow-hidden z-50">
         {/* Logo */}
-        <div className="flex w-fit h-full flex-row items-center ml-24 mt-2">
+        <div className="flex w-fit h-full flex-row items-center ml-5 lg:ml-24 mt-2">
           <img
             src={navlogo}
             alt="accs-logo"
@@ -21,10 +22,10 @@ function App() {
         <div className="w-full flex justify-end">
           {/* Home , Customer , Service , Contact */}
           <div className="flex flex-row space-x-10 mr-5">
-            <p className="cursor-pointer">หน้าหลัก</p>
-            <p className="cursor-pointer">ลูกค้าของเรา</p>
-            <p className="cursor-pointer">บริการของเรา</p>
-            <p className="cursor-pointer">ติดต่อเรา</p>
+            <p className="cursor-pointer whitespace-nowrap">หน้าหลัก</p>
+            <p className="cursor-pointer whitespace-nowrap">ลูกค้าของเรา</p>
+            <p className="cursor-pointer whitespace-nowrap">บริการของเรา</p>
+            <p className="cursor-pointer whitespace-nowrap">ติดต่อเรา</p>
           </div>
           <div className="flex flex-row space-x-2 ml-4 mr-16 items-center select-none cursor-pointer">
             {/* Flag */}
@@ -36,7 +37,7 @@ function App() {
               />
             </div>
             {/* Language */}
-            <p>ภาษาไทย</p>
+            <p className="whitespace-nowrap cursor-pointer">ภาษาไทย</p>
             {/* Dropdown Icon */}
             <div>
               <svg
@@ -57,10 +58,19 @@ function App() {
         </div>
       </div>
       {/* body */}
-      <div className="flex w-full h-full bg-red-200">
+      <div className="flex w-full h-full">
         {/* Carousel */}
-        <div className="flex bg-blue-100 min-w-full">
-          <img src={carouselbg} alt="carouselbg" className="object-cover min-w-full blur-sm" />
+        <div className="flex relative min-w-full w-full h-fit min-h-[450px] overflow-hidden">
+          <img
+            src={carouselBG}
+            alt="carouselbg"
+            className="object-cover min-w-full"
+          />
+          <img
+            src={carouselPropOne}
+            alt="standingGirl"
+            className="absolute -right-9 sm:right-0 lg:right-[20%] -bottom-11 sm:bottom-6 scale-75 sm:scale-110 sm:h-5/6 object-cover"
+          />
         </div>
       </div>
     </div>
