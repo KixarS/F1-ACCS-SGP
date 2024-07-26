0a34a0e1a93b2f04f879ac5730b54ad7f8267cf9
KixarS <teoylimhao2@gmail.com>
2024-07-27 01:15:06 +0700

    mobile nav

--- src/App.jsx
+++ src/App.jsx
@@ -108,33 +108,88 @@ function App() {
           )}
           onClick={HamNavOpenHandle}
         >
-          <nav className="flex mt-[79px] flex-col space-y-10 items-start text-[24px] h-full min-w-fit duration-300 font-medium ">
+          <nav className="flex mt-[79px] flex-col space-y-8 items-start text-[24px] h-full min-w-fit duration-300 font-medium">
+            {/* home */}
             <a
               href="#home"
-              className="flex relative h-fit items-center min-w-fit justify-center duration-300 underline-button "
+              className="flex relative h-10 items-center min-w-fit justify-center duration-300 lg:underline-button"
             >
-              <div>
-
+              <div className="flex flex-row justify-start items-end space-x-5">
+                <div className="flex justify-center items-center h-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-10 h-9"
+                    viewBox="0 0 26 26"
+                  >
+                    <path d="M 20 2.03125 C 19.449219 2.03125 19 2.480469 19 3.03125 L 19 7.8125 L 13.71875 2.53125 C 13.328125 2.140625 12.671875 2.140625 12.28125 2.53125 L 0.5625 14.28125 C 0.171875 14.671875 0.171875 15.296875 0.5625 15.6875 C 0.953125 16.078125 1.578125 16.078125 1.96875 15.6875 L 13 4.65625 L 24.0625 15.71875 C 24.257813 15.914063 24.523438 16.03125 24.78125 16.03125 C 25.039063 16.03125 25.273438 15.914063 25.46875 15.71875 C 25.859375 15.328125 25.859375 14.703125 25.46875 14.3125 L 22 10.84375 L 22 3.03125 C 22 2.480469 21.550781 2.03125 21 2.03125 Z M 13 6.5 L 2 17.5 L 2 23 C 2 24.65625 3.34375 26 5 26 L 21 26 C 22.65625 26 24 24.65625 24 23 L 24 17.5 Z M 11 16 L 15 16 C 15.550781 16 16 16.449219 16 17 L 16 23 C 16 23.550781 15.550781 24 15 24 L 11 24 C 10.449219 24 10 23.550781 10 23 L 10 17 C 10 16.449219 10.449219 16 11 16 Z"></path>
+                  </svg>
+                </div>
+                <div className="flex h-full flex-row justify-start items-center">
+                  หน้าหลัก
+                </div>
               </div>
-              <p>หน้าหลัก</p>
             </a>
+            {/* partner */}
             <a
               href="#sponsor"
-              className="flex relative h-fit items-center min-w-fit justify-center duration-300 underline-button"
+              className="flex relative h-10 items-center min-w-fit justify-center duration-300 lg:underline-button"
             >
-              ลูกค้าของเรา
+              <div className="flex flex-row justify-start items-start space-x-5">
+                <div className="flex justify-center items-center">
+                  <svg
+                    className="w-10 h-10 mt-1"
+                    viewBox="5 0 60 60"
+                    data-name="Layer 1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      class="cls-1"
+                      d="M64,12.78v17s-3.63.71-4.38.81-3.08.85-4.78-.78C52.22,27.25,42.93,18,42.93,18a3.54,3.54,0,0,0-4.18-.21c-2.36,1.24-5.87,3.07-7.33,3.78a3.37,3.37,0,0,1-5.06-2.64,3.44,3.44,0,0,1,2.1-3c3.33-2,10.36-6,13.29-7.52,1.78-1,3.06-1,5.51,1C50.27,12,53,14.27,53,14.27a2.75,2.75,0,0,0,2.26.43C58.63,14,64,12.78,64,12.78ZM27,41.5a3,3,0,0,0-3.55-4.09,3.07,3.07,0,0,0-.64-3,3.13,3.13,0,0,0-3-.75,3.07,3.07,0,0,0-.65-3,3.38,3.38,0,0,0-4.72.13c-1.38,1.32-2.27,3.72-1,5.14s2.64.55,3.72.3c-.3,1.07-1.2,2.07-.09,3.47s2.64.55,3.72.3c-.3,1.07-1.16,2.16-.1,3.46s2.84.61,4,.25c-.45,1.15-1.41,2.39-.18,3.79s4.08.75,5.47-.58a3.32,3.32,0,0,0,.3-4.68A3.18,3.18,0,0,0,27,41.5Zm25.35-8.82L41.62,22a3.53,3.53,0,0,0-3.77-.68c-1.5.66-3.43,1.56-4.89,2.24a8.15,8.15,0,0,1-3.29,1.1,5.59,5.59,0,0,1-3-10.34C29,12.73,34.09,10,34.09,10a6.46,6.46,0,0,0-5-2C25.67,8,18.51,12.7,18.51,12.7a5.61,5.61,0,0,1-4.93.13L8,10.89v19.4s1.59.46,3,1a6.33,6.33,0,0,1,1.56-2.47,6.17,6.17,0,0,1,8.48-.06,5.4,5.4,0,0,1,1.34,2.37,5.49,5.49,0,0,1,2.29,1.4A5.4,5.4,0,0,1,26,34.94a5.47,5.47,0,0,1,3.71,4,5.38,5.38,0,0,1,2.39,1.43,5.65,5.65,0,0,1,1.48,4.89,0,0,0,0,1,0,0s.8.9,1.29,1.39a2.46,2.46,0,0,0,3.48-3.48s2,2.48,4.28,1c2-1.4,1.69-3.06.74-4a3.19,3.19,0,0,0,4.77.13,2.45,2.45,0,0,0,.13-3.3s1.33,1.81,4,.12c1.89-1.6,1-3.43,0-4.39Z"
+                    />
+                  </svg>
+                </div>
+                <div className="flex h-10 flex-row justify-start items-center">
+                  พาร์ทเนอร์
+                </div>
+              </div>
             </a>
             <a
               href="#service"
-              className="flex relative h-fit items-center min-w-fit justify-center duration-300 underline-button"
+              className="flex relative h-fit items-center min-w-fit justify-center duration-300 lg:underline-button"
             >
-              บริการของเรา
+              <div className="flex flex-row justify-start items-start space-x-5">
+                <div className="flex justify-center items-center h-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8"
+                    viewBox="0 0 26 26"
+                  >
+                    <path d="M 20 2.03125 C 19.449219 2.03125 19 2.480469 19 3.03125 L 19 7.8125 L 13.71875 2.53125 C 13.328125 2.140625 12.671875 2.140625 12.28125 2.53125 L 0.5625 14.28125 C 0.171875 14.671875 0.171875 15.296875 0.5625 15.6875 C 0.953125 16.078125 1.578125 16.078125 1.96875 15.6875 L 13 4.65625 L 24.0625 15.71875 C 24.257813 15.914063 24.523438 16.03125 24.78125 16.03125 C 25.039063 16.03125 25.273438 15.914063 25.46875 15.71875 C 25.859375 15.328125 25.859375 14.703125 25.46875 14.3125 L 22 10.84375 L 22 3.03125 C 22 2.480469 21.550781 2.03125 21 2.03125 Z M 13 6.5 L 2 17.5 L 2 23 C 2 24.65625 3.34375 26 5 26 L 21 26 C 22.65625 26 24 24.65625 24 23 L 24 17.5 Z M 11 16 L 15 16 C 15.550781 16 16 16.449219 16 17 L 16 23 C 16 23.550781 15.550781 24 15 24 L 11 24 C 10.449219 24 10 23.550781 10 23 L 10 17 C 10 16.449219 10.449219 16 11 16 Z"></path>
+                  </svg>
+                </div>
+                <div className="flex h-10 flex-row justify-start items-end">
+                  บริการของเรา
+                </div>
+              </div>
             </a>
             <a
               href="#contact"
-              className="flex relative h-fit items-center min-w-fit justify-center duration-300 underline-button"
+              className="flex relative h-fit items-center min-w-fit justify-center duration-300 lg:underline-button"
             >
-              ติดต่อเรา
+              <div className="flex flex-row justify-start items-start space-x-5">
+                <div className="flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8"
+                    viewBox="0 0 26 26"
+                  >
+                    <path d="M 20 2.03125 C 19.449219 2.03125 19 2.480469 19 3.03125 L 19 7.8125 L 13.71875 2.53125 C 13.328125 2.140625 12.671875 2.140625 12.28125 2.53125 L 0.5625 14.28125 C 0.171875 14.671875 0.171875 15.296875 0.5625 15.6875 C 0.953125 16.078125 1.578125 16.078125 1.96875 15.6875 L 13 4.65625 L 24.0625 15.71875 C 24.257813 15.914063 24.523438 16.03125 24.78125 16.03125 C 25.039063 16.03125 25.273438 15.914063 25.46875 15.71875 C 25.859375 15.328125 25.859375 14.703125 25.46875 14.3125 L 22 10.84375 L 22 3.03125 C 22 2.480469 21.550781 2.03125 21 2.03125 Z M 13 6.5 L 2 17.5 L 2 23 C 2 24.65625 3.34375 26 5 26 L 21 26 C 22.65625 26 24 24.65625 24 23 L 24 17.5 Z M 11 16 L 15 16 C 15.550781 16 16 16.449219 16 17 L 16 23 C 16 23.550781 15.550781 24 15 24 L 11 24 C 10.449219 24 10 23.550781 10 23 L 10 17 C 10 16.449219 10.449219 16 11 16 Z"></path>
+                  </svg>
+                </div>
+                <div className="flex h-10 flex-row justify-start items-end">
+                  ติดต่อเรา
+                </div>
+              </div>
             </a>
           </nav>
         </div>
@@ -307,7 +362,7 @@ function App() {
         </section>
       </div>
       {/* footer */}
-      {/* <section className="flex relative h-[550px] bg-white flex-col ">
+      <section className="flex relative h-[550px] bg-white flex-col ">
         <div className="flex w-full h-full flex-row">
           <div className="flex flex-col items-center w-6/12 justify-center">
             <img
@@ -349,7 +404,7 @@ function App() {
             />
           </div>
         </button>
-      </section> */}
+      </section>
     </div>
   );
 }
